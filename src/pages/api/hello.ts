ac723388ef0b950c66ed384e2e3da7ba7e474c9a
shichisan21 <photograph.by@gmail.com>
2023-10-04 00:09:35 +0900

    CORSエラーになっている

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -15,9 +15,21 @@ import { NextApiRequest, NextApiResponse } from "next";
  * Program
  */
 
+// Next.js API Route
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  // Set CORS headers
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173"); // Or your client app's origin
   res.setHeader("Access-Control-Allow-Methods", "GET, POST");
-  console.log("req", req);
-  res.status(200).json({ name: "John Doe" });
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  // Handle the request
+  if (req.method === "POST") {
+    // Your POST handler logic
+    res.status(200).json({ status: "success" });
+  } else if (req.method === "GET") {
+    // Your GET handler logic
+    res.status(200).json({ name: "John Doe" });
+  } else {
+    res.status(405).json({ error: "Method not allowed" });
+  }
 }
