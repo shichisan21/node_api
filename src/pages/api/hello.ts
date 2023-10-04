37733782522a48fd7f691e75443979cff5a58638
shichisan21 <photograph.by@gmail.com>
2023-10-05 00:08:54 +0900

    APIレスポンス取得

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -18,13 +18,20 @@ import { NextApiRequest, NextApiResponse } from "next";
 // Next.js API Route
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   // Set CORS headers
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173"); // Or your client app's origin
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
 
+  // Add this to handle CORS preflight requests
+  if (req.method === "OPTIONS") {
+    res.status(204).end();
+    return;
+  }
   // Handle the request
   if (req.method === "POST") {
     // Your POST handler logic
+    console.log("post", req.body);
     res.status(200).json({ status: "success" });
   } else if (req.method === "GET") {
     // Your GET handler logic
