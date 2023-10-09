3a22a88a49d982e4aee0d43fc59df0e14b8bd9d9
shichisan21 <photograph.by@gmail.com>
2023-10-09 22:56:13 +0900

    chatGPT req

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -2,6 +2,7 @@
  * import
  */
 import { NextApiRequest, NextApiResponse } from "next";
+import axios from "axios";
 
 /***
  *Interface & Type
@@ -16,7 +17,10 @@ import { NextApiRequest, NextApiResponse } from "next";
  */
 
 // Next.js API Route
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
   // Set CORS headers
   res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST");
@@ -31,8 +35,41 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
   // Handle the request
   if (req.method === "POST") {
     // Your POST handler logic
-    console.log("post", req.body);
-    res.status(200).json({ status: "success" });
+
+    const userInput = req.body.key1;
+
+    let definitionText = "日本語で回答してください";
+
+    console.log(userInput);
+
+    const payload = {
+      model: "gpt-3.5-turbo",
+      messages: [
+        { role: "assistant", content: definitionText },
+        { role: "user", content: userInput },
+      ],
+      temperature: 0.9,
+      max_tokens: 300,
+    };
+
+    try {
+      const openaiResponse = await axios.post(
+        "https://api.openai.com/v1/chat/completions",
+        payload,
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${process.env.CHAT_GPT_KEY}`,
+          },
+        }
+      );
+      res
+        .status(200)
+        .json({ message: openaiResponse.data.choices[0].message.content });
+    } catch (error: any) {
+      console.error("Error calling OpenAI API:", error?.response?.data);
+      res.status(500).json({ error: "Error calling OpenAI API" });
+    }
   } else if (req.method === "GET") {
     // Your GET handler logic
     res.status(200).json({ name: "John Doe" });
